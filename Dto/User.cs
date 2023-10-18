c9582afb30aea66caad2b76996a7234c05cc0233
elishvaM <148370420+elishvaM@users.noreply.github.com>
2023-10-18 20:37:20 +0300

    עם שרה 2

--- Dto/User.cs
+++ Dto/User.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 
 namespace Dto;
+public partial class UserLogin
+{
+    public string Password { get; set; } = "";
 
-public partial class UserDto
+    public string Email { get; set; } = "";
+
+}
+public partial class FullUser:UserDto
+{
+    public string Password { get; set; } = "";
+
+}
+public partial class UserDto 
 {
     public int Id { get; set; }
 
     public string Name { get; set; } = "";
 
     public string Phone { get; set; } = "";
-
-    public string Password { get; set; } = "";
 
     public string Email { get; set; } = "";
 
