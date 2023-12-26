f0bccf3cfd187e43ee0bca195da6127ca118f813
elishvaM <148370420+elishvaM@users.noreply.github.com>
2023-12-26 19:25:49 +0200

    הוספת תגובה לא גמור

---
+++ Dal/CommentDal.cs
@@ -0,0 +1,42 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class CommentDal : ICommentDal
+    {
+        private readonly ElishevaMHadasBListsTripContext ElishevaMHadasBListsTripContext;
+        public CommentDal(ElishevaMHadasBListsTripContext context)
+        {
+            this.ElishevaMHadasBListsTripContext = context;
+        }
+        public void Add(Comment comment)
+        {
+            ElishevaMHadasBListsTripContext.Comments.Add(comment);
+            ElishevaMHadasBListsTripContext.SaveChanges();
+        }
+
+        public void Delete(int commentId)
+        {
+            Comment foundComment = ElishevaMHadasBListsTripContext.Comments.FirstOrDefault(x => x.Id == commentId);
+           ElishevaMHadasBListsTripContext.Comments.Remove(foundComment);
+            ElishevaMHadasBListsTripContext.SaveChanges();
+
+        }
+
+        //public List<Comment> GetByAttractionId(int attractionId)
+        //{
+        //    return "jj";
+        //    //return ElishevaMHadasBListsTripContext.Comments.FirstOrDefault(x=>x.)
+        //}
+
+        public List<Comment> GetComplained()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dal/ICommentDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+namespace Dal
+{
+    public interface ICommentDal
+    {
+        //List<Comment> GetByAttractionId(int attractionId);
+        List<Comment> GetComplained();
+        //??האם טוב
+        void Delete(int commentId);
+        void Add(Comment comment);  
+    }
+}
